eb577195b655d712f5c59735797cfde4fe6ceed5
meedrop <339282292@qq.com>
2016-02-05 11:01:31 +0800

    commit

---
+++ test_tcp/test_tcp_server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import socket
+import threading
+import time
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(('127.0.0.1',9999))
+s.listen(5)
+print('Waitting for connection...')
+
+def tcplink(sock,addr):
+    print('Accept new connection from %s:%s' % addr)
+    socket.send(b'Welcome!')
+    while True:
+        data=sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello,%s!' % data.decode('utf-8')).encode('utf-8'))
+        sock.close()
+        print('Connection from %s:%s closed' % addr)
+
+while True:
+    sock,addr=s.accept()
+    t=threading.Thread(target=tcplink,args=(sock,addr))
+    t.start()
